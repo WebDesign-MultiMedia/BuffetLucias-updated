ce5d0429a79983fc07339ea7b73c44fae17ba8b8
WebDesign-MultiMedia <110946138+WebDesign-MultiMedia@users.noreply.github.com>
2024-04-06 01:35:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.alert("the words in are RED are not links. The words in WHITE are links to images displaying its discription!");
+window.alert("the words in  RED are not links. The words in WHITE are links to images displaying its discription!");
